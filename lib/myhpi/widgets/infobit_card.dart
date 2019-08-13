fee516f72be88d4829e48f1d4701fbf4e02baf1c
clemens <clemens.tiedt@student.hpi.de>
2019-08-13 19:02:38 +0200

    feat(myhpi): add InfoBit action functionality

--- lib/myhpi/widgets/infobit_card.dart
+++ lib/myhpi/widgets/infobit_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:grpc/grpc.dart';
 import 'package:hpi_flutter/myhpi/bloc.dart';
 import 'package:hpi_flutter/myhpi/data/infobit.dart';
 import 'package:hpi_flutter/myhpi/data/infobit.dart' as prefix0;
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoBitCard extends StatelessWidget {
   final InfoBit infoBit;
@@ -13,28 +15,59 @@ class InfoBitCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          Text(
-            infoBit.title,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          Text(infoBit.description),
-          Wrap(
-            children: infoBit.actionIds
-                .map((a) => StreamBuilder<prefix0.Action>(
-                      stream: MyHPIBloc(Provider.of<ClientChannel>(context))
-                          .getAction(a),
-                      builder: (context, snapshot) {
-                        return ActionChip(
-                          label: Text(snapshot.data.title),
-                          onPressed: () {},
-                        );
-                      },
-                    ))
-                .asList(),
-          )
-        ],
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              infoBit.title,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              infoBit.description,
+              style: Theme.of(context).textTheme.subhead,
+            ),
+            Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 8.0,
+              children: infoBit.actionIds
+                  .map((a) => StreamBuilder<prefix0.Action>(
+                        stream: MyHPIBloc(Provider.of<ClientChannel>(context))
+                            .getAction(a),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData)
+                            return Chip(label: Text('Loading...'));
+                          return ActionChip(
+                            label: Text(snapshot.data.title),
+                            onPressed: () async {
+                              if (snapshot.data is ActionText) {
+                                var content =
+                                    (snapshot.data as ActionText).content;
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return Scaffold(
+                                    appBar: AppBar(
+                                        title: Text(snapshot.data.title)),
+                                    body: Html(
+                                      data: content,
+                                    ),
+                                  );
+                                }));
+                              } else if (snapshot.data is ActionLink) {
+                                var url = (snapshot.data as ActionLink).url;
+                                if (await canLaunch(url)) await launch(url);
+                              }
+                            },
+                          );
+                        },
+                      ))
+                  .asList(),
+            )
+          ],
+        ),
       ),
     );
   }
