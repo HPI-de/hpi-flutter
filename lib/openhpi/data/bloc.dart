d88223bb263a5795204725ebd391b7af69a2b11a
Jonas Wanke <contact@jonas-wanke.com>
2019-08-15 16:23:28 +0200

    fix(openhpi): support UTF-8

--- lib/openhpi/data/bloc.dart
+++ lib/openhpi/data/bloc.dart
@@ -7,25 +7,35 @@ import 'dart:async';
 import 'course.dart';
 
 const String COURSE_URL = 'https://open.hpi.de/api/v2/courses';
+const String HEADER_ACCEPT = 'Accept';
+const String OPEN_HPI_ACCEPT = 'application/vnd.api+json; xikolo-version=3.7';
 
 @immutable
-class OpenHpiCourseBloc {
-  Future<String> fetchData(String link) async {
-    final response = await http.get(Uri.parse(link));
+class OpenHpiBloc {
+  Future<String> fetchData(String link, {Map<String, String> headers}) async {
+    final response = await http.get(Uri.parse(link), headers: headers);
     if (response.statusCode == 200) {
       // If server returns an OK response, return the response body.
-      return response.body;
+      return Encoding.getByName('UTF-8').decode(response.bodyBytes);
     } else {
       // If that response was not OK, throw an error.
-      throw Exception('Failed to load data');
+      throw Exception(
+          'Failed to load data, status code: ${response.statusCode} ($response)');
     }
   }
 
   Stream<KtList<OpenHpiCourse>> getOpenHpiCourses() {
-    return Stream.fromFuture(fetchData(COURSE_URL).then((jsonString) =>
-        KtList.from(jsonDecode(jsonString)['data'] as Iterable).map((course) =>
-            OpenHpiCourse.fromJson(
-                course['attributes'] as Map<String, dynamic>))));
+    return Stream.fromFuture(
+      fetchData(
+        COURSE_URL,
+        headers: {HEADER_ACCEPT: OPEN_HPI_ACCEPT},
+      ).then((jsonString) =>
+          KtList.from(jsonDecode(jsonString)['data'] as Iterable).map(
+            (course) => OpenHpiCourse.fromJson(
+              course['attributes'] as Map<String, dynamic>,
+            ),
+          )),
+    );
   }
 
   Stream<KtList<OpenHpiCourse>> getAnnouncedOpenHpiCourses() {
